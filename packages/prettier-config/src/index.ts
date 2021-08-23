5fbb8bf756f0109c7b2c3d7e2274dce727b6dba8
김용건 (Kim Younggeon) <mail@kyg.kr>
2021-08-23 15:03:29 +0900

    chore: initial commit

---
+++ packages/prettier-config/src/index.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  proseWrap: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+}
